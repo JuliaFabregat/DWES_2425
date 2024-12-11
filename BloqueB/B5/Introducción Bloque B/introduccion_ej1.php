57eb83f590c266c4409a8da10bb7a684db423abb
Julia Fabregat <juliapfdam@outlook.com>
2024-12-11 11:22:44 +0100

    Realización del ej Introducción

---
+++ BloqueB/B5/Introducción Bloque B/introduccion_ej1.php
@@ -0,0 +1,34 @@
+<?php
+// Variables
+$username = 'Icy';
+$user_array = [
+    'name'=>'Icy',
+    'age'=>24,
+    'active'=>true,
+];
+
+class User{
+    // Propiedades
+    public $name;
+    public $age;
+    public $active;
+
+    // Constructor
+    public function __construct($name, $age, $active){
+        $this->name = $name;
+        $this->age = $age;
+        $this->active = $active;
+    }
+}
+
+// MAIN
+$user_object = new User('Icy', 24, true);
+
+?>
+
+<!-- HTML -->
+<!-- Con PRE englobando todo el texto -->
+<pre>Scalar: <?php var_dump($username); ?> </pre>
+<!-- Con PRE solo englobando la salida de PHP -->
+<p>Array: <pre> <?php var_dump($user_array); ?> </pre> </p>
+<p>Object: <pre> <?php var_dump($user_object); ?> </pre> </p>
