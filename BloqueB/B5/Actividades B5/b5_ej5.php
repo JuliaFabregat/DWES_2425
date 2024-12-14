cc8be45fc48b9a23e81cc76a5e4fac5ed5301951
Julia Fabregat <juliapfdam@outlook.com>
2024-12-14 13:51:58 +0100

    Creación de actividades 4 y 5

---
+++ BloqueB/B5/Actividades B5/b5_ej5.php
@@ -0,0 +1,8 @@
+<?php 
+// $reg = '/[a-z0-9-_.]+@[a-z]+.+(es|com)/';        -> Con el último paréntesis puede ser .es o .com SOLO
+// $reg2 = '/[a-z0-9-_.]+@[a-z]+.+[a-z]{2,3}/';     -> Con el último paréntesis puede ser cualquier otra terminación de 2 o 3 letras
+
+// Variables
+$exprReg = '/[a-z0-9-_.]+@[a-z]+.+(es|com)/';
+
+?>
